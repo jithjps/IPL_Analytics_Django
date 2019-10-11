5756890e7d4dd4d55897244fefd8daa774a3ad6d
JithJPS <56298826+jithjps@users.noreply.github.com>
2019-10-11 12:55:25 +0530

    all errors in Views changed

--- IPL_Analytics/ipl_data/views.py
+++ IPL_Analytics/ipl_data/views.py
@@ -26,9 +26,9 @@ def  top4teams_by_wins(request):
                                     order_by('-win_count')
     if not winner_result:
         return JsonResponse({"response":"No data available"})
-    top_4_win_count = winner_result.values_list('win_count',flat=True).distinct()[:4]
+    top_4_win_count = winner_result.values_list('win_count',flat=True).distinct().order_by('-win_count')[:4]
     win_count4      = list(top_4_win_count)[-1]
-    top_4_teams     = winner_result.filter(win_count__gte = win_count4)
+    top_4_teams     = winner_result.filter(win_count__gte = win_count4).order_by('-win_count')
     
     # Top Team by wincounts 
     top_win_count   = top_4_win_count[0]
@@ -37,16 +37,16 @@ def  top4teams_by_wins(request):
 
     # Top team highest victory locations
     win_venues          = Match.objects.filter(season_id = season_id, winner__in =  top_team_names).\
-                                        values('team','venue').\
+                                        values('winner','venue').\
                                         annotate(venue_count = Count('venue')).\
                                         order_by('-venue_count')
     top_win_venue_data = []
     for top_team_name in top_team_names:   
         win_venues  = Match.objects.filter(season_id = season_id, winner =  top_team_name).\
-                                    values('venue','Team').\
+                                    values('winner','venue').\
                                     annotate(venue_count = Count('venue')).\
                                     order_by('-venue_count')                                 
-        top_win_venue_count = win_venues.filter(values_list('venue_count',flat = True).first()
+        top_win_venue_count = win_venues.values_list('venue_count',flat = True).first()
         top_win_venues      = win_venues.filter(venue_count = top_win_venue_count)
         top_win_venue_data.extend(top_win_venues)
 
@@ -60,10 +60,10 @@ def  top4teams_by_wins(request):
     top_mom_player  = mom_top_players.filter(win_count = mom_top_count)
 
     return JsonResponse ({
-                'top_4_teams'    : top_4_teams,
-                'top_team'       : top_team,
-                'top_win_venues' : top_win_venue_data,
-                'top_mom_player' : top_mom_player
+                'top_4_teams'    : list(top_4_teams),
+                'top_team'       : list(top_team),
+                'top_win_venues' : list(top_win_venue_data),
+                'top_mom_player' : list(top_mom_player)
             },safe=False)
 
 @csrf_exempt
@@ -81,7 +81,8 @@ def toss_analytics(request):
     # How many times has a team won the toss and the match for the selected season
     team_wise_toss_match_win = Match.objects.filter(season_id = season_id, winner = F('toss_winner')).\
                                               values('winner').\
-                                              annotate(team_count = Count('winner'))
+                                              annotate(team_count = Count('winner')).\
+                                              order_by('-team_count')
 
 
     # Which team won the most number of tosses in the season
@@ -94,8 +95,8 @@ def toss_analytics(request):
 
     return JsonResponse ({
             'chose_to_bat_percentage'   : chose_to_bat_percentage,
-            'team_wise_toss_match_win'  : team_wise_toss_match_win,
-            'top_toss_win_team'         : top_toss_win_team
+            'team_wise_toss_match_win'  : list(team_wise_toss_match_win),
+            'top_toss_win_team'         : list(top_toss_win_team)
            },safe=False)
 
 @csrf_exempt
@@ -116,8 +117,8 @@ def  victory_analysis(request):
     top_team_by_wkt_margin = margin_of_run_wkts.filter(win_by_wickets = top_margin_of_wkts)
 
     return JsonResponse ({ 
-                "top_team_by_run_margin" : top_team_by_run_margin,
-                "top_team_by_wkt_margin" : top_team_by_wkt_margin
+                "top_team_by_run_margin" : list(top_team_by_run_margin),
+                "top_team_by_wkt_margin" : list(top_team_by_wkt_margin)
            },safe=False)
 
 
@@ -131,8 +132,7 @@ def location_analytics(request):
                                       annotate(venue_count = Count('venue')).\
                                       order_by('-venue_count')
     top_hosting_count = match_by_venue.values_list('venue_count', flat = True).first()
-    to_hosting_venues = match_by_venue.filter(venue_count = top_hosting_count )
-    top_venue_names    = to_hosting_venues.values_list('venue',flat=True)
+    top_venue_names = match_by_venue.filter(venue_count = top_hosting_count ).values_list('venue',flat=True)
 
     venue_status_dict = {}
     for _venue in top_venue_names:
@@ -159,11 +159,9 @@ def location_analytics(request):
                 if item in  team_status:
                     team_status[item]['tie_or_no_result'] += 1
         venue_status_dict[_venue] = team_status
-
-    return  JsonResponse ({
-               "top_venue_names": top_venue_names,
-               "venue_status_dict" :venue_status_dict
-    },safe=False)  
+    return  JsonResponse ({ "venue_status_dict" : venue_status_dict,
+                            "top_hosting_count" : top_hosting_count
+                           },safe=False)  
 
 
 
